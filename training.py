ffd91b62ae4e9c790b2f5cce867c3f7a67cacdd5
usmanjalil1 <usmanjalil1@gmail.com>
2024-03-05 00:03:24 +0500

    Updating requirements.txt

--- training.py
+++ training.py
@@ -11,7 +11,6 @@ mlp.fit(X_train, y_train)
 y_pred = mlp.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy score of MLP Classifier:", format(accuracy*100, '.2f'), "%")
-
 # Training the decision tree classifier
 clf = TreeClassifier()
 clf.fit(X_train, y_train)
