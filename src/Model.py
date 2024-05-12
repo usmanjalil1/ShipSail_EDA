5c5e09fe9b156fa5ae3fc53a519c7c9471995755
usmanjalil1 <usmanjalil1@gmail.com>
2024-05-12 15:57:01 +0500

    adding rfc

--- src/Model.py
+++ src/Model.py
@@ -1,11 +1,15 @@
 from sklearn.neural_network import MLPClassifier
 from sklearn.base import ClassifierMixin
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 def MLP(hidden_layer: tuple, iteration: int, verbose: bool) -> ClassifierMixin:
     mlp = MLPClassifier(hidden_layer_sizes=hidden_layer, max_iter=iteration, random_state=42, verbose=verbose)
     return mlp
 
+def ForestClassifier() -> ClassifierMixin:
+    rfc = RandomForestClassifier()
+    return rfc
 
 def TreeClassifier() -> ClassifierMixin:
     dt = DecisionTreeClassifier()
--- training.py
+++ training.py
@@ -1,20 +1,19 @@
 from src import pipeline
-from src import MLP, TreeClassifier
+from src import MLP, TreeClassifier, ForestClassifier
 from src import HIDDEN_LAYER_SIZE, ITER, VERBOSE
 from sklearn.metrics import accuracy_score
 import warnings
 warnings.filterwarnings("ignore")
 
 X_train, X_test, y_train, y_test = pipeline()
-mlp = MLP(HIDDEN_LAYER_SIZE, ITER, VERBOSE)
-mlp.fit(X_train, y_train)
-y_pred = mlp.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-print("Accuracy score of MLP Classifier:", format(accuracy*100, '.2f'), "%")
+# mlp = MLP(HIDDEN_LAYER_SIZE, ITER, VERBOSE)
+# mlp.fit(X_train, y_train)
+# y_pred = mlp.predict(X_test)
+# accuracy = accuracy_score(y_test, y_pred)
+# print("Accuracy score of MLP Classifier:", format(accuracy*100, '.2f'), "%")
 # Training the decision tree classifier
-clf = TreeClassifier()
+clf = ForestClassifier()
 clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
-
-print("Accuracy score of Decision Tree Classifier:", format(accuracy*100, '.2f'), "%")
+print("Accuracy score of  Random Forest Classifier:", format(accuracy*100, '.2f'), "%")
